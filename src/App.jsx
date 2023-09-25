3ee4e1bd6e38d7f1532aa20d92b2a2013a75fedf
Leandro-Montenegro <leanmontenegro2929@gmail.com>
2023-09-25 20:19:37 -0300

    segunda entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
+import { useState } from "react"
+import './style.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import Navbar from './components/Navbar/Navbar'
+import Categories from './components/categories/Categories'
 
 function App() {
   const greeting = "Bienvenido/a, en esta tienda encontraras ropa, accesorios y mangas para satisfacer tus necesidades."
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <Navbar />
       <ItemListContainer greeting={greeting}/>
-
+      <Categories/>
     </>
   )
 }
