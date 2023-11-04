0baf2150619837d2d91ffe7fec3292ef84439a7d
Leandro-Montenegro <leanmontenegro2929@gmail.com>
2023-11-04 20:05:49 -0300

    entrega final

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -5,31 +5,72 @@ import { db } from "../../firebase/config";
 
 
 const Checkout = () => {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        alert("Orden generada con éxito");
+
+        setName("");
+        setPhone("");
+        setEmail("");
+    };
     return (
         <div>
             <h2>Terminar compra</h2>
-            <h5>por favor llenar datos</h5>
-            <form>
+            <h5>Por favor llenar datos</h5>
+            <form onSubmit={handleSubmit}>
                 <div className="mb">
                     <label className="form-label">Nombre completo</label>
-                    <input className="form-control" type="text" placeholder="Nombre y apellido" name="name" /> 
+                    <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Nombre y apellido"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div className="mb">
                     <label className="form-label">Numero de telefono</label>
-                    <input className="form-control" type="number" placeholder="5435189426489" name="Phone" /> 
+                    <input
+                        className="form-control"
+                        type="number"
+                        placeholder="5435189426489"
+                        name="Phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
                 </div>
                 <div className="mb">
-                    <label className="form-label">email</label>
-                    <input className="form-control" type="email" placeholder="lalala@lala.com" name="mail" /> 
+                    <label className="form-label">Email</label>
+                    <input
+                        className="form-control"
+                        type="email"
+                        placeholder="lalala@lala.com"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="mb">
                     <label className="form-label">repita su email</label>
-                    <input className="form-control" type="email" placeholder="lalala@lala.com" name="mail" /> 
+                    <input
+                        className="form-control"
+                        type="email"
+                        placeholder="lalala@lala.com"
+                        name="mail" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <button className="btn btn-dark" type="submit">Generar orden</button>
             </form>
         </div>
     )
-    }
+}
 export default Checkout;
 
